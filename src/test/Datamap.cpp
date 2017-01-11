041d1888ced3634929bbed18d8d4ab73862cc892
PoulainJulien <poulain-julien@hotmail.com>
2017-01-11 07:02:07 +0100

    Add classes that makes possible to write something like for (int i : range(n))

--- src/test/Datamap.cpp
+++ src/test/Datamap.cpp
@@ -1,24 +1,16 @@
 #include "Datamap.hpp"
+#include "Range.hpp"
 
 void printmap(Map & map)
 {
-    int i = 0, j = 0, w = map.GetWidth(), h = map.GetHeight();
-    while (i < w)
+    int w = map.GetWidth(), h = map.GetHeight();
+    for (int i : range(w))
     {
-        j = 0;
-        while (j < h)
-        {
+        for (int j : range(h))
             if (map.IsWall(i, j))
-            {
                 printf("%c", 219);
-            }
             else
-            {
                 printf(" ");
-            }
-            j++;
-        }
         printf("\n");
-        i++;
     }
 }
